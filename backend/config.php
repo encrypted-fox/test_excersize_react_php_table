c718f6dbef1513a741b4656f6577372ef873ff98
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-07-03 20:47:02 +0300

    Missing PHP classes

--- backend/config.php
+++ backend/config.php
@@ -1,7 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 
-
-class Connect
+class Connect extends PDO
 {
-
+    public function __construct()
+    {
+        parent::__construct("mysql:host=localhost;dbname=api", 'mysql', 'mysql',
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
 }
